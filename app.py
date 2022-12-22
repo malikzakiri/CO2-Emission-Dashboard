9412b8a6bf89c5f47f48118100648f5b0d9820f0
Ardhi <71832020+malikzakiri@users.noreply.github.com>
2022-12-22 22:13:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,39 +19,48 @@ app = dash.Dash(
 )
 
 # -- Navbar & Title Tab --
-app.title='CO2 Emissions'
+app.title='CO₂ Emissions'
 navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavItem(dbc.NavLink("Home", href="#")),
-    ],
-    brand="Carbon Dioxide Emissions Around The Globe",
-    brand_href="#",
+    brand="Mental Health - Suicide Dashboard Analytics",
     color="primary",
     dark=True,
-    brand_style={'fontSize' : '28px'},
-    style={'fontSize':'20px'},
+    brand_style={'fontSize' : '24px'}
 )
 
-# -- IMPORT & QUERYING DATASET CO2 --
+# -- IMPORT & QUERYING DATASET CO₂ --
 df1=pd.read_csv('owid-co2-data.csv')
 df = df1[['country', 'year', 'iso_code', 'co2_per_capita']]
 df.drop(df[df['year'] < 1980].index, inplace=True)
 
 # -- CHOROPLETH --
-df_choropleth = df.rename(columns = {'co2_per_capita':'CO2 Per Capita'})
+df_choropleth = df.rename(columns = {'co2_per_capita':'CO₂ Per Capita (Tonnes)'})
 plot_map= px.choropleth(df_choropleth,
     locations='iso_code',
     color_continuous_scale='OrRd',
-    color='CO2 Per Capita',
+    color='CO₂ Per Capita (Tonnes)',
     range_color=[0, 20],
     animation_frame='year',
     labels={
         'year':'Year'
     },
+    title='Per Capita CO₂ Emissions, 1980-2021',
     hover_name='country',
     hover_data={
         'iso_code': False,
         'year' : False
+    }
+)
+
+plot_map.update_layout(
+    title={
+        'text': "Per Capita CO₂ Emissions, 1980-2021",
+        'y':0.96,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    
+    font={
+        'color':'black'
     }
 )
 
@@ -64,18 +73,18 @@ line_chart = px.line(
     y="co2_per_capita",
     color='country',
     labels={
-        'co2_per_capita': 'CO2 Per Capita (Tonnes)',
+        'co2_per_capita': 'CO₂ Per Capita (Tonnes)',
         'year' : 'Year',
         'country' : 'Country'
     },
-    title='The Trend of CO2 Emissions Per Capita Around World',
+    title='The Trend of CO₂ Emissions Per Capita Around World',
     template='seaborn'
 )
 
 line_chart.update_layout(hovermode="x")
 line_chart.update_traces(
     hovertemplate= 
-    "CO2 Per Capita: %{y:.2f} Tonne"
+    "CO₂ Per Capita: %{y:.2f} Tonne"
 )
 
 # --------- BAR ---------- 
@@ -88,16 +97,15 @@ bar_plot = px.bar(
     x='co2_per_capita',
     labels={
         'country' : 'Country',
-        'co2_per_capita' : 'CO2 Per Capita (Tonnes)'
+        'co2_per_capita' : 'CO₂ Per Capita (Tonnes)'
     },
-    title='Top 10 Country with The Highest CO2 Emission per Capita',
+    title='Top 10 Country with The Highest CO₂ Emission Per Capita',
     
     #template='seaborn'
 )
 bar_plot.update_traces(
     hovertemplate= 
-    "<b>%{y}</b><br><br>"
-    "CO2 Per Capita: %{x:.2f} Tonne"    
+    "CO₂ Per Capita: %{x:.2f} Tonne"    
 )
 
 # ------------------- PIE ---------------
@@ -119,25 +127,30 @@ pie_plot = px.pie(
     values='value',
     names='variable',
     color='variable',
-    title='CO2',
+    title='CO₂',
     template='plotly',
     color_discrete_sequence=px.colors.qualitative.Plotly,
 )
 pie_plot.update_traces(
     hovertemplate= 
     "<b>%{label}</b><br><br>"
-    "CO2 (in million Tonnes): %{value:,.2f}",
+    "CO₂ (in million Tonnes): %{value:,.2f}",
     textinfo='label+percent',
     textfont_size=14
 )
 
 pie_plot.update_layout(
     title={
-        'text': "The Source of CO2 Emissions in 2021",
+        'text': "The Source of CO₂ Emissions in 2021",
         'y':0.9,
         'x':0.47,
         'xanchor': 'center',
-        'yanchor': 'top'})
+        'yanchor': 'top'},
+
+    font={
+        'color':'black'
+    }
+)
 
 # ---------------------- LAYOUT ----------------------------
 app.layout = html.Div(children=[
@@ -151,7 +164,7 @@ app.layout = html.Div(children=[
         dbc.Row([
             dbc.Col([
                 dbc.Card([
-                    dbc.CardHeader('Carbon Dioxide (CO2) Gas Emission'),
+                    dbc.CardHeader("Carbon Dioxide CO₂ Gas Emission"),
                     dbc.CardBody([
                         html.P("Emissions of greenhouse gases, predominantly carbon dioxide, have been steadily increasing and kicking the greenhouse effect out of balance. What does this mean? Essentially, there are too many greenhouse gases absorbing the sun's energy, which means our planet is slowly warming up. We know this as climate change.", 
                             style={
@@ -159,7 +172,7 @@ app.layout = html.Div(children=[
                                 'fontFamily' : 'Helvetica'
                             }
                         ),
-                        html.P('Carbon dioxide (CO2) is a colourless, odourless and non-poisonous gas formed by combustion of carbon and in the respiration of living organisms and is considered a greenhouse gas. Emissions means the release of greenhouse gases and/or their precursors into the atmosphere over a specified area and period of time. Carbon dioxide emissions or CO2 emissions are emissions stemming from the burning of fossil fuels and the manufacture of cement; they include carbon dioxide produced during consumption of solid, liquid, and gas fuels as well as gas flaring.',
+                        html.P('Carbon dioxide (CO₂) is a colourless, odourless and non-poisonous gas formed by combustion of carbon and in the respiration of living organisms and is considered a greenhouse gas. Emissions means the release of greenhouse gases and/or their precursors into the atmosphere over a specified area and period of time. Carbon dioxide emissions or CO₂ emissions are emissions stemming from the burning of fossil fuels and the manufacture of cement; they include carbon dioxide produced during consumption of solid, liquid, and gas fuels as well as gas flaring.',
                             style={
                                 'textAlign' : 'justify',
                                 'fontFamily' : 'Helvetica'
@@ -172,9 +185,9 @@ app.layout = html.Div(children=[
                             }
                         ),
                         dbc.CardLink('Our World in Data ', href='https://ourworldindata.org/per-capita-co2'),
-                        dbc.CardLink('Eurostat Statistics Explained', href='https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Glossary:Carbon_dioxide_emissions#:~:text=Carbon%20dioxide%20(CO2)%20is,area%20and%20period%20of%20time.'),
+                        dbc.CardLink('Eurostat Statistics Explained', href='https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Glossary:Carbon_dioxide_emissions#:~:text=Carbon%20dioxide%20(CO₂)%20is,area%20and%20period%20of%20time.'),
                         #html.P('Carbon dioxide is a chemical compound made up of molecules that each have one carbon atom covalently double bonded to two oxygen atoms. It is found in the gas state at room temperature.'),
-                        #html.P("In the air, carbon dioxide is transparent to visible light but absorbs infrared radiation, acting as a greenhouse gas. It is a trace gas in Earth's atmosphere at 417 ppm (about 0.04%) by volume, having risen from pre-industrial levels of 280 ppm. Burning fossil fuels is the primary cause of these increased CO2 concentrations and also the primary cause of global warming and climate change. Carbon dioxide is soluble in water and is found in groundwater, lakes, ice caps, and seawater. When carbon dioxide dissolves in water it forms carbonic acid (H2CO3), which causes ocean acidification as atmospheric CO2 levels increase.")
+                        #html.P("In the air, carbon dioxide is transparent to visible light but absorbs infrared radiation, acting as a greenhouse gas. It is a trace gas in Earth's atmosphere at 417 ppm (about 0.04%) by volume, having risen from pre-industrial levels of 280 ppm. Burning fossil fuels is the primary cause of these increased CO₂ concentrations and also the primary cause of global warming and climate change. Carbon dioxide is soluble in water and is found in groundwater, lakes, ice caps, and seawater. When carbon dioxide dissolves in water it forms carbonic acid (H2CO3), which causes ocean acidification as atmospheric CO₂ levels increase.")
                     ])
                 ])
             ],
@@ -194,7 +207,7 @@ app.layout = html.Div(children=[
             html.Br(),
             html.Br(),
             dbc.Col([
-                html.H4('Anlysis by Country', style={'paddingTop':'10px'}),
+                html.H4('Anlaysis by Country', style={'paddingTop':'10px'}),
                 dbc.Tabs([
                     dbc.Tab([
                         dcc.RadioItems(
@@ -238,10 +251,13 @@ app.layout = html.Div(children=[
             width=6
             ),
             dbc.Col([
-                html.H4('Anlysis by The Source', style={'paddingTop':'10px'}),
+                html.H4('Analysis by The Source', style={'paddingTop':'10px'}),
                 dcc.Graph(
                     id='pie_chart',
                     figure=pie_plot,
+                    style={
+                        'paddingTop':'65px'
+                    }
                 )
             ],
             width=6
@@ -268,18 +284,18 @@ def update_plot1(country_list):
         y="co2_per_capita",
         color='country',
         labels={
-            'co2_per_capita': 'CO2 Per Capita (Tonnes)',
+            'co2_per_capita': 'CO₂ Per Capita (Tonnes)',
             'year' : 'Year',
             'country' : 'Country'
         },
-        title='CO2 Trend',
+        title='CO₂ Trend',
         template='seaborn'
     )
 
     line_chart.update_layout(hovermode="x")
     line_chart.update_traces(
         hovertemplate= 
-        "CO2 Per Capita: %{y:.2f} Tonne"
+        "CO₂ Per Capita: %{y:.2f} Tonne"
     )
 
     return line_chart
@@ -295,10 +311,10 @@ def update_plot1(bar_sort):
             data_frame=df_bar.tail(10),
             y='country',
             x='co2_per_capita',
-            title='Top 10 Country with The Highest CO2 Emission per Capita',
+            title='Top 10 Country with The Highest CO₂ Emission Per Capita',
             labels={
                 'country' : 'Country',
-                'co2_per_capita' : 'CO2 Per Capita (Tonnes)'
+                'co2_per_capita' : 'CO₂ Per Capita (Tonnes)'
             },
             template='seaborn'
         )
@@ -307,18 +323,17 @@ def update_plot1(bar_sort):
             data_frame=df_bar.head(10),
             y='country',
             x='co2_per_capita',
-            title='Top 10 Country with The Lowest CO2 Emission per Capita',
+            title='Top 10 Country with The Lowest CO₂ Emission per Capita',
             labels={
                 'country' : 'Country',
-                'co2_per_capita' : 'CO2 Per Capita (Tonnes)'
+                'co2_per_capita' : 'CO₂ Per Capita (Tonnes)'
             },
             template='seaborn'
         )
 
     bar_plot.update_traces(
         hovertemplate= 
-        "<b>%{y}</b><br><br>"
-        "CO2 Per Capita: %{x:.2f} Tonne"
+        "CO₂ Per Capita: %{x:.2f} Tonne"
         
     )
     
